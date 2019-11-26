e7e2145bffb73d4dfed36fc3b7734ce20e06ff01
Belaynesh Hailu <bhailu103@gmail.com>
2019-11-26 11:46:26 -0600

    modified file and Adding Contact page on the Sample App

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
     def hello
-        render html: "Hello World"
+        render html: "Hello, World"
+    end
+    def home
+        render html: "Home"
+
+    end
+    def help
+        render html: "Help"
+
+    end
+    def about
+        render html: "about"
+    end
+    def title
+        render html: "Title"
+    end
 end
